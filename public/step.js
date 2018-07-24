09a8754214939788fcb62bb95295b3adb5af2119
Nhat Thanh <thanhdntage@gmail.com>
2018-07-25 00:47:43 +0700

    add

---
+++ public/step.js
@@ -0,0 +1,8 @@
+let home = document.getElementById("left");
+let home1 = document.getElementById("right");
+let flag = document.getElementById("wrapper").querySelectorAll(".tests");
+let flag1 = document.querySelectorAll("#wrapper .tests");
+let flag3 = document.querySelector("#wrapper");
+let just = document.querySelectorAll('.tests');
+
+home.style.backgroundColor = "red";
